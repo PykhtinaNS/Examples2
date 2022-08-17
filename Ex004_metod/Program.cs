dfc614e5f19eb50746dc3fb53f4a871cfdb14bbe
Natalya Pykhtina <sovanst4@yandex.ru>
2022-08-17 14:51:03 +0300

    Ex004_2

--- Ex004_metod/Program.cs
+++ Ex004_metod/Program.cs
@@ -1,10 +1,10 @@
 ﻿void FillArray(int[] collection)
 {
-    int length=collection.Length;
-    int index=0;
-    while(index<length)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        collection[index]=new Random().Next(1,10);
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
@@ -12,12 +12,31 @@ void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while(position<count)
+    while (position < count)
     {
         Console.WriteLine(col[position]);
         position++;
     }
 }
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+int pos = Indexof(array,4);
+Console.WriteLine(pos);
