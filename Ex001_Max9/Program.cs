852a3e804fb986c22cef7f824a05ca82515d99f3
Natalya Pykhtina <sovanst4@yandex.ru>
2022-08-17 13:25:41 +0300

    Ex001

---
+++ Ex001_Max9/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+int a1=12;
+int b1=23;
+int c1=12;
+int a2=125;
+int b2=23;
+int c2=61;
+int a3=2;
+int b3=232;
+int c3=10;
+int max=Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+Console.WriteLine(max);
