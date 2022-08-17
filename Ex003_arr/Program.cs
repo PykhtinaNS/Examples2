80dc6cccbd8ff61483edee4b0fbcd03a818e6e05
Natalya Pykhtina <sovanst4@yandex.ru>
2022-08-17 14:01:26 +0300

    Ex003

---
+++ Ex003_arr/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 3, 4, 1, 23, 98, 0, 1, 760, 543 };
+int n = array.Length;
+int find = 1;
+int index = 0;
+while(index<n)
+{
+    if (array[index]==find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
